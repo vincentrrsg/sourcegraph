8ed6b79e5d2488fdbb9b16ae740c285b1d0ffc29
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2022-12-23 18:11:25 +0530

    WIP Save

--- internal/repos/check_connection.go
+++ internal/repos/check_connection.go
@@ -3,20 +3,16 @@ package repos
 import (
 	"net"
 	"net/url"
+	"time"
 
+	"github.com/sourcegraph/log"
 	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
 
-// checkConnection parses the rawURL and makes a best effort attempt to obtain a hostname. It then
-// performs an IP lookup on that hostname and returns a non-nil error on failure.
-//
-// At the moment this function is only limited to doing IP lookups. We may want/have to expand this
-// to support other code hosts or to add more checks (for example making a test API call to verify
-// the authorization, etc).
-func checkConnection(rawURL string) error {
+func getHostname(rawURL string) (string, error) {
 	u, err := url.Parse(rawURL)
 	if err != nil {
-		return errors.Wrap(err, "invalid or bad url for connection check")
+		return "", errors.Wrap(err, "invalid or bad url for connection check")
 	}
 
 	// Best effort at finding a hostname. For example if rawURL is sourcegraph.com, then u.Host is
@@ -33,18 +29,73 @@ func checkConnection(rawURL string) error {
 			// rawURL is most likely something like "sourcegraph.com:80", read from u.Path.
 			hostname = u.Path
 		} else {
-			return errors.Newf("unsupported url format (%q) for connection check", rawURL)
+			return "", errors.Newf("unsupported url format (%q) for connection check", rawURL)
 		}
+	}
+
+	return hostname, nil
+}
+
+// checkConnection parses the rawURL and makes a best effort attempt to obtain a hostname. It then
+// performs an IP lookup on that hostname and returns a non-nil error on failure.
+//
+// At the moment this function is only limited to doing IP lookups. We may want/have to expand this
+// to support other code hosts or to add more checks (for example making a test API call to verify
+// the authorization, etc).
+func checkConnection(rawURL string) error {
+	logger := log.Scoped("checkConnection", "")
+
+	if err := dnsLookup(logger, rawURL); err != nil {
+		return errors.Wrap(err, "DNS lookup failed")
+	}
+
+	if err := ping(logger, rawURL); err != nil {
+		return errors.Wrap(err, "ping failed")
+	}
+
+	return nil
+}
+
+func dnsLookup(logger log.Logger, rawURL string) error {
+	hostname, err := getHostname(rawURL)
+	if err != nil {
+		return errors.Wrap(err, "getHostname failed")
 	}
 
 	ips, err := net.LookupIP(hostname)
 	if err != nil {
-		return errors.Wrap(err, "connection check failed")
+		return errors.Wrap(err, "DNS check failed")
 	}
 
 	if len(ips) == 0 {
-		return errors.Newf("connection check failed, no IP addresses found for hostname %q", hostname)
+		return errors.Newf("DNS check failed, no IP addresses found for hostname %q", hostname)
 	}
 
 	return nil
+}
+
+// ping attempts to connect to the given rawURL. Technically it is not exactly a ping request in the
+// UNIX sense since it uses TCP instead of ICMP. But we use the name to signifiy the intent here,
+// which is check if we can connect to the URL.
+func ping(logger log.Logger, rawURL string) error {
+	var address string
+
+	// Check if rawURL includes a port in it.
+	_, port, err := net.SplitHostPort(rawURL)
+	if err != nil {
+		// If no port is set, default to 80.
+		address = net.JoinHostPort(rawURL, "80")
+	} else {
+		// We need to split the protocol from rawURL.
+		hostname, err := getHostname(rawURL)
+		if err != nil {
+			return errors.Wrap(err, "getHostname failed")
+		}
+
+		address = net.JoinHostPort(hostname, port)
+	}
+
+	logger.Warn("Dialing with timeout (1s)", log.String("address", address))
+	_, err = net.DialTimeout("tcp", address, time.Duration(time.Second))
+	return err
 }
--- internal/repos/check_connection_test.go
+++ internal/repos/check_connection_test.go
@@ -1,40 +1,68 @@
 package repos
 
-import "testing"
+import (
+	"testing"
 
-func TestCheckConnection(t *testing.T) {
+	"github.com/sourcegraph/log"
+)
+
+func TestDnsLookup(t *testing.T) {
+	logger := log.Scoped("dnsLookupTest", "")
+
 	t.Run("bad URL", func(t *testing.T) {
-		if err := checkConnection("foo"); err == nil {
+		if err := dnsLookup(logger, "foo"); err == nil {
 			t.Error("Expected error but got nil")
 		}
 	})
 
 	t.Run("good URL", func(t *testing.T) {
-		if err := checkConnection("https://sourcegraph.com"); err != nil {
+		if err := dnsLookup(logger, "https://sourcegraph.com"); err != nil {
 			t.Errorf("Expected nil but got error: %v", err)
 		}
 	})
 
 	t.Run("good URL with port", func(t *testing.T) {
-		if err := checkConnection("https://sourcegraph.com:80"); err != nil {
+		if err := dnsLookup(logger, "https://sourcegraph.com:80"); err != nil {
 			t.Errorf("Expected nil but got error: %v", err)
 		}
 	})
 
 	t.Run("good URL without protocol", func(t *testing.T) {
-		if err := checkConnection("sourcegraph.com"); err != nil {
+		if err := dnsLookup(logger, "sourcegraph.com"); err != nil {
 			t.Errorf("Expected nil but got error: %v", err)
 		}
 	})
 
 	t.Run("good URL with port but without protocol", func(t *testing.T) {
-		if err := checkConnection("sourcegraph.com:80"); err != nil {
+		if err := dnsLookup(logger, "sourcegraph.com:80"); err != nil {
 			t.Errorf("Expected nil but got error: %v", err)
 		}
 	})
 
 	t.Run("good URL with username:password", func(t *testing.T) {
-		if err := checkConnection("https://username:password@sourcegraph.com"); err != nil {
+		if err := dnsLookup(logger, "https://username:password@sourcegraph.com"); err != nil {
+			t.Errorf("Expected nil but got error: %v", err)
+		}
+	})
+}
+
+func TestPing(t *testing.T) {
+	logger := log.Scoped("pingTest", "")
+
+	t.Run("hostname and port", func(t *testing.T) {
+		if err := ping(logger, "sourcegraph.com:80"); err != nil {
+			t.Errorf("Expected nil but got error: %v", err)
+		}
+	})
+
+	t.Run("hostname and no port", func(t *testing.T) {
+		if err := ping(logger, "ghe.sgdev.org"); err != nil {
+			t.Errorf("Expected nil but got error: %v", err)
+		}
+	})
+
+	t.Run("protocol and hostname", func(t *testing.T) {
+		if err := ping(logger, "https://ghe.sgdev.org"); err != nil {
 			t.Errorf("Expected nil but got error: %v", err)
 		}
 	})

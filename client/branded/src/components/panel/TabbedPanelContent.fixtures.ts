5250f7b040b2d6e8c1406267a1dbd3b40205a7fd
Quinn Slack <quinn@slack.org>
2022-12-26 20:33:57 -0600

    use new non-extensions-based panel, remove old HierarchicalLocationsView

--- client/branded/src/components/panel/TabbedPanelContent.fixtures.ts
+++ client/branded/src/components/panel/TabbedPanelContent.fixtures.ts
@@ -1,15 +1,8 @@
-import { noop } from 'lodash'
-import { EMPTY, NEVER, of } from 'rxjs'
+import { of } from 'rxjs'
 
-import { FlatExtensionHostAPI } from '@sourcegraph/shared/src/api/contract'
-import { ExtensionCodeEditor } from '@sourcegraph/shared/src/api/extension/api/codeEditor'
-import { ExtensionDocument } from '@sourcegraph/shared/src/api/extension/api/textDocument'
-import { PanelViewData } from '@sourcegraph/shared/src/api/extension/extensionHostApi'
-import { pretendProxySubscribable, pretendRemote } from '@sourcegraph/shared/src/api/util'
 import { NOOP_TELEMETRY_SERVICE } from '@sourcegraph/shared/src/telemetry/telemetryService'
-import { extensionsController } from '@sourcegraph/shared/src/testing/searchTestHelpers'
 
-export const panels: PanelViewData[] = [
+export const panels = [
     {
         id: 'panel_1',
         title: 'Panel 1',
@@ -75,32 +68,4 @@ export const panelProps = {
     platformContext: {} as any,
     settingsCascade: { subjects: null, final: null },
     telemetryService: NOOP_TELEMETRY_SERVICE,
-    extensionsController: {
-        ...extensionsController,
-        extHostAPI: Promise.resolve(
-            pretendRemote<FlatExtensionHostAPI>({
-                getContributions: () => pretendProxySubscribable(NEVER),
-                registerContributions: () => pretendProxySubscribable(EMPTY).subscribe(noop as any),
-                haveInitialExtensionsLoaded: () => pretendProxySubscribable(of(true)),
-                getPanelViews: () => pretendProxySubscribable(of(panels)),
-                getActiveViewComponentChanges: () => pretendProxySubscribable(of(CODE_EDITOR_FIXTURE)),
-                getActiveCodeEditorPosition: () => pretendProxySubscribable(NEVER),
-            })
-        ),
-    },
 }
-
-export const CODE_EDITOR_FIXTURE = new ExtensionCodeEditor(
-    {
-        type: 'CodeEditor',
-        viewerId: 'viewer#0',
-        resource: 'git://foo?1#/bar.go',
-        selections: [],
-        isActive: true,
-    },
-    new ExtensionDocument({
-        uri: 'git://foo?1#/bar.go',
-        languageId: 'go',
-        text: 'type My[Kingdom For] Generics',
-    })
-)
